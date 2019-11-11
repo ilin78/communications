43ef92cef2d67ff232f6e3a4e6069e671c86cff2
ilinoa <ilinoa90@gmail.com>
2019-11-11 15:08:46 +0300

    add user for message

--- chat/src/pages/Home/index.jsx
+++ chat/src/pages/Home/index.jsx
@@ -4,13 +4,20 @@ import {Message} from 'components';
 import './Home.scss';
 
 const Home = () => (
-    <section > 
-        <Message 
-        avatar="https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_tijjpyw1qe201.png" 
-        text="HelloHelloHell oHelloHello HelloHelloHelloHelloHe lloHelloHelloHelloHelloHe llo HelloHelloHelloHelloHelloHel loHelloHelloHelloHelloHelloHel oHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello!"
-        date="Fri Nov 08 2019 18:27:48 GMT+0300 (Москва, стандартное время) {}"/>
-    </section>  
-    
+    <section >
+        <Message
+            avatar="https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_tijjpyw1qe201.png" 
+            text="HelloHelloHell oHelloHello HelloHelloHelloHello"
+            date={new Date('Thu Aug 29 2019 11:22:58')}
+        ></Message>
+        <Message
+            avatar="https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_tijjpyw1qe201.png" 
+            text="H!"
+            date={new Date('Thu Aug 29 2019 11:22:58')}
+            isMe={true}
+            isReaded={true}
+        ></Message>
+    </section>    
 );
 
 export default Home;
